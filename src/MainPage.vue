c3b20c963f344fafe5f41389e004a8473aa53593
AnnetteScott <notnatural21@gmail.com>
2023-09-10 21:19:41 +1200

    Add new case funtion

--- src/MainPage.vue
+++ src/MainPage.vue
@@ -7,6 +7,22 @@ class UserData {
 
 class Case {
 	number = 1;
+	locations = {
+		"CHEMIST": {revealed: false},
+		"BANK": {revealed: false},
+		"CARRIAGE DEPOT": {revealed: false},
+		"DOCKS": {revealed: false},
+		"HOTEL": {revealed: false},
+		"LOCKSMITH": {revealed: false},
+		"MUSEUM": {revealed: false},
+		"NEWSAGENT": {revealed: false},
+		"PARK": {revealed: false},
+		"PAWNBROKER": {revealed: false},
+		"THEATRE": {revealed: false},
+		"BOAR'S HEAD": {revealed: false},
+		"SCOTLAND YARD": {revealed: false},
+		"TOBACCONIST": {revealed: false},
+	}
 }
 
 export default defineComponent({
@@ -14,6 +30,7 @@ export default defineComponent({
 	data() {
 		return {
 			userData: {} as UserData,
+			caseNum: 1,
 		};
 	},
 	mounted(){
@@ -34,6 +51,13 @@ export default defineComponent({
 		update(){
 			localStorage.setItem('userData', JSON.stringify(this.userData));
 		},
+		newCase(){
+			const newCase = new Case();
+			newCase.number = this.caseNum;
+
+			this.userData.cases.push(newCase);
+			this.update();
+		}
 	},
 })
 
@@ -41,10 +65,46 @@ export default defineComponent({
 
 <template>
 	<main>
-
+		<section>
+			<h1>New Case</h1>
+			<form @submit="newCase()">
+				<input type="number" min="1" max="75" step="1" v-model="caseNum">
+				<button type="submit">Create Case</button>
+			</form>
+		</section>
+		<section>
+			<h1>Past Cases</h1>
+		</section>
 	</main>
 </template>
 
 <style scoped>
+section {
+	width: 100%;
+	display: flex;
+    flex-direction: column;
+    align-items: center;
+	gap: 10px;
+}
 
+form {
+	width: 50%;
+	display: flex;
+    flex-direction: column;
+    gap: 12px;
+}
+
+form > input {
+	border-radius: 8px;
+    border: 1px solid black;
+	height: 25px;
+}
+
+button {
+	background-color: #027be3;
+	color: white;
+	border: 1px solid black;
+	border-radius: 8px;
+	padding: 5px 20px;
+}
 </style>
